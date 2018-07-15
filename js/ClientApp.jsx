b4588286464a17e62fa5b44c01d40d9a7dc1ccae
vasil-dimov <vasil.dimov.work@gmail.com>
2018-07-15 14:36:37 +0300

    2. Tools - ESLint

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,18 +1,25 @@
 const ce = React.createElement;
 
 const MyTitle = function(props) {
-  return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
+  return ce(
+    "div",
+    null,
+    ce("h1", { style: { color: props.color } }, props.title)
+  );
 };
 
 const MyFirstComponent = function() {
   return ce(
-    'div',
-    { id: 'my-first-component' },
-    ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
-    ce(MyTitle, { title: 'Stranger Things', color: 'GreenYellow' }),
-    ce(MyTitle, { title: 'Rick and Morty', color: 'LimeGreen' }),
-    ce(MyTitle, { title: 'Silicon Valley', color: 'peru' })
+    "div",
+    { id: "my-first-component" },
+    ce(MyTitle, { title: "Game of Thrones", color: "YellowGreen" }),
+    ce(MyTitle, { title: "Stranger Things", color: "GreenYellow" }),
+    ce(MyTitle, { title: "Rick and Morty", color: "LimeGreen" }),
+    ce(MyTitle, { title: "Silicon Valley", color: "peru" })
   );
 };
 
-ReactDOM.render(React.createElement(MyFirstComponent), document.getElementById('app'));
+ReactDOM.render(
+  React.createElement(MyFirstComponent),
+  document.getElementById("app")
+);
