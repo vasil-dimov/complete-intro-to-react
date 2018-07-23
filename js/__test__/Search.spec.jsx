334eeb9da9b542e8483af31402817562537c906b
vasil-dimov <vasil.dimov.work@gmail.com>
2018-07-23 20:02:22 +0300

    5. Testing React

---
+++ js/__test__/Search.spec.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { shallow } from "enzyme";
+import preload from "../../data.json";
+import Search from "../Search";
+import ShowCard from "../ShowCard";
+
+test("Search renders correctly", () => {
+  const component = shallow(<Search />);
+  expect(component).toMatchSnapshot();
+});
+
+test("Search should render correct of shows", () => {
+  const component = shallow(<Search />);
+  expect(component.find(ShowCard).length).toEqual(preload.shows.length);
+});
+
+test("Search should render correct amount of shows based on search term", () => {
+  const searchWord = "black";
+  const component = shallow(<Search />);
+  component.find("input").simulate("change", { target: { value: searchWord } });
+  const showCount = preload.shows.filter(show =>
+    `${show.title.toUpperCase()} ${show.description.toUpperCase()}`.includes(
+      searchWord.toUpperCase()
+    )
+  ).length;
+  expect(showCount).toEqual(component.find(ShowCard).length);
+});
