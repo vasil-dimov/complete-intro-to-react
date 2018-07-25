f080d89298ac4e2086547881281120dbc0001a22
vasil-dimov <vasil.dimov.work@gmail.com>
2018-07-25 19:16:01 +0300

    8.Data in Reac - Url Parameters

--- js/App.jsx
+++ js/App.jsx
@@ -1,14 +1,31 @@
 // @flow
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import type { Match } from "react-router-dom";
 import Landing from "./Landing";
 import Search from "./Search";
+import Details from "./Details";
+import preload from "../data.json";
+
+const FourOhFour = () => <h1>404</h1>;
 
 const App = () => (
   <BrowserRouter>
     <div className="app">
-      <Route exact path="/" component={Landing} />
-      <Route path="/search" component={Search} />
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route path="/search" component={Search} />
+        <Route
+          path="/details/:id"
+          component={(props: { match: Match }) => {
+            const selectedShow = preload.shows.find(
+              (show: Show) => props.match.params.id === show.imdbID
+            );
+            return <Details show={selectedShow} {...props} />;
+          }}
+        />
+        <Route component={FourOhFour} />
+      </Switch>
     </div>
   </BrowserRouter>
 );
