0ddbbe93505ae5e2be445dd3d82becf36a542536
vasil-dimov <vasil.dimov.work@gmail.com>
2018-07-25 20:18:33 +0300

    8.Data in Reac - Fix Search Tests

--- js/__test__/Search.spec.jsx
+++ js/__test__/Search.spec.jsx
@@ -1,3 +1,5 @@
+// @flow
+
 import React from "react";
 import { shallow } from "enzyme";
 import preload from "../../data.json";
@@ -5,23 +7,24 @@ import Search from "../Search";
 import ShowCard from "../ShowCard";
 
 test("Search renders correctly", () => {
-  const component = shallow(<Search />);
+  const component = shallow(<Search shows={preload.shows} />);
   expect(component).toMatchSnapshot();
 });
 
-test("Search should render correct of shows", () => {
-  const component = shallow(<Search />);
+test("Search should render correct amount of shows", () => {
+  const component = shallow(<Search shows={preload.shows} />);
   expect(component.find(ShowCard).length).toEqual(preload.shows.length);
 });
 
 test("Search should render correct amount of shows based on search term", () => {
   const searchWord = "black";
-  const component = shallow(<Search />);
+  const component = shallow(<Search shows={preload.shows} />);
   component.find("input").simulate("change", { target: { value: searchWord } });
-  const showCount = preload.shows.filter(show =>
-    `${show.title.toUpperCase()} ${show.description.toUpperCase()}`.includes(
-      searchWord.toUpperCase()
-    )
+  const showCount = preload.shows.filter(
+    show =>
+      `${show.title} ${show.description}`
+        .toUpperCase()
+        .indexOf(searchWord.toUpperCase()) >= 0
   ).length;
-  expect(showCount).toEqual(component.find(ShowCard).length);
+  expect(component.find(ShowCard).length).toEqual(showCount);
 });
